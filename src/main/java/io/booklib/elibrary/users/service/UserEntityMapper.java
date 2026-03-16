a425631ce169323e3c1523c8faa5c653c4224f55
Lady R'n'B <rahma.barrak.k@gmail.com>
2026-03-16 17:42:38 +0100

    Encode the password in the DB

--- src/main/java/io/booklib/elibrary/users/service/UserEntityMapper.java
+++ src/main/java/io/booklib/elibrary/users/service/UserEntityMapper.java
@@ -1,15 +1,23 @@
 package io.booklib.elibrary.users.service;
 
 import io.booklib.elibrary.users.repository.UserEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserEntityMapper {
 
+    private final PasswordEncoder passwordEncoder;
+
+    public UserEntityMapper (PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
+
     public UserEntity mapToEntity(UserDTO userDTO){
-        return new UserEntity(userDTO.userId(), userDTO.username(), userDTO.password(), userDTO.email());
+        return new UserEntity(userDTO.userId(), userDTO.username(), passwordEncoder.encode(userDTO.password()), userDTO.email());
     }
 
     public UserDTO mapToDTO(UserEntity userEntity){
-        return new UserDTO(userEntity.getId(), userEntity.getUsername(), userEntity.getPassword(), userEntity.getEmail());
+        return new UserDTO(userEntity.getId(), userEntity.getUsername(), null, userEntity.getEmail());
     }
-
 }
--- src/main/java/io/booklib/elibrary/users/service/UserService.java
+++ src/main/java/io/booklib/elibrary/users/service/UserService.java
@@ -2,16 +2,18 @@ package io.booklib.elibrary.users.service;
 
 import io.booklib.elibrary.users.repository.UserEntity;
 import io.booklib.elibrary.users.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
-    private final UserEntityMapper mapper = new UserEntityMapper();
+    private final UserEntityMapper mapper;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,  UserEntityMapper mapper) {
         this.userRepository = userRepository;
+        this.mapper = mapper;
     }
 
     public UserDTO registerUser(UserDTO userDTO) {
